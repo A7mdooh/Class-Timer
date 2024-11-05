79f81ac4f1d0a48f4f176d7f3bc1893a0c1eb385
A7mdooh <80976049+A7mdooh@users.noreply.github.com>
2024-11-05 11:39:56 +0400

    Update eventsData.js

--- eventsData.js
+++ eventsData.js
@@ -2,6 +2,6 @@
 const eventsData = [
     { day: "الأحد", name: "حصة رياضيات", start: "09:00 AM", end: "10:00 AM", teacher: "الأستاذ علي", class: "الصف الخامس" },
     { day: "الإثنين", name: "حصة علوم", start: "10:15 AM", end: "11:15 AM", teacher: "الأستاذة فاطمة", class: "الصف السادس" },
-    { day: "الثلاثاء", name: "حصة لغة عربية", start: "08:00 AM", end: "09:00 AM", teacher: "الأستاذ محمد", class: "الصف الرابع" },
+    { day: "الثلاثاء", name: "حصة لغة عربية", start: "11:40 AM", end: "11:41 AM", teacher: "الأستاذ محمد", class: "الصف الرابع" },
     { day: "الأربعاء", name: "حصة تربية إسلامية", start: "11:00 AM", end: "12:00 PM", teacher: "الأستاذة عائشة", class: "الصف الثالث" }
 ];
